43180f0e4999eef27abe4ea311797fd9d0ee9bb6
aaron butler <aaron butler>
2015-11-20 12:31:06 -0500

    create an index2.html file that uses feedreader2.js which only tests done placements

--- jasmine/spec/feedreader2.js
+++ jasmine/spec/feedreader2.js
@@ -1,16 +1,33 @@
 $(function() {
-    describe('Initial Entries', function(){
+    describe('it()', function(){
 		beforeEach(function(done){
 			loadFeed(0,function(){
 				done();
 			});
-			//done();
 		});
 		
-		it('has at least a single .entry within the .feed container',function() {
+		it('a. explicitly passes done as a parameter and invokes done',function(done) {
+			var entry = $('.feed .entry')[0];
+			expect(entry).toBeGreaterThan('');
+			done();
+		});
+		
+		it('b. explicitly passes done as a parameter, but doesnt invoke done',function(done) {
 			var entry = $('.feed .entry')[0];
 			expect(entry).toBeGreaterThan('');
 			//done();
+		});
+		
+		it('c. doesnt explicitly pass done as a parameter, but does invoke done',function() {
+			var entry = $('.feed .entry')[0];
+			expect(entry).toBeGreaterThan('');
+			done();
+		});
+		
+		it('d. never mentions done',function() {
+			var entry = $('.feed .entry')[0];
+			expect(entry).toBeGreaterThan('');
+
 		});
 		
 	});
